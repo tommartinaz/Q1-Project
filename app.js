f560ea35f560c632ee31ebe87a8a12122a3f26b6
Tom Martin <tom.martinaz@gmail.com>
2017-06-07 09:04:02 -0700

    Basic functionality working - one row

--- app.js
+++ app.js
@@ -19,14 +19,8 @@ var curPlayer;
 $.get("https://deckofcardsapi.com/api/deck/" + deckID + "/shuffle/").done(function() {
   var curVal = 0;
   var nextCard = 0;
-  var player1 = [{
-      name: "Tom",
-      id: 1
-    }],
-    player2 = [{
-      name: "Aaron",
-      id: 2
-    }];
+  var player1 = [],
+    player2 = [];
   var slotCounter = 1;
   var tempIndex = 0;
   var frozenIndex = 0;
@@ -41,21 +35,22 @@ $.get("https://deckofcardsapi.com/api/deck/" + deckID + "/shuffle/").done(functi
     })
   };
   var compareCards = function(arr, index, btn) {
-    var rowSlot = "#p" + arr[0].id + "s";
-    if (btn === "higher" && arr[index + 1] < arr[index + 2] || btn === "lower" && arr[index + 1] > arr[index + 2]) {
-      alert("That's right!");
-      return index + 1;
+    var rowSlot = "#p1s";
+    if (btn === "higher" && arr[index] < arr[index + 1] || btn === "lower" && arr[index] > arr[index + 1]) {
+      //alert("That's right!");
+      index++;
+      return index;
     } else {
-      alert("Sorry, wrong answer");
+      //alert("Sorry, wrong answer");
 
       //arr = arr.slice(0, frozenIndex + 2);
       return 0;
     }
   };
   $("#begin").click(begin);
-  var redrawCardBacks = function(player, slot) {
+  var redrawCardBacks = function(slot) {
     for (var j = (slot || 2); j < 6; j++) {
-      $("#p" + player[0].id + "s" + j).css('background-image', 'url("images/card_back.jpg")');
+      $("#p1s" + j).css('background-image', 'url("images/card_back.jpg")');
     };
   };
 
@@ -63,7 +58,6 @@ $.get("https://deckofcardsapi.com/api/deck/" + deckID + "/shuffle/").done(functi
     console.log($(event.target));
     var btnValue = $(event.target).attr("value");
     var rowSlot = "#p1s"
-    curPlayer = player1;
     var cardSlot = frozenIndex + 1 + tempIndex;
     switch (btnValue) {
       case "higher":
@@ -72,23 +66,24 @@ $.get("https://deckofcardsapi.com/api/deck/" + deckID + "/shuffle/").done(functi
           nextCard = data2.cards[0];
           //console.log(nextCard);
           nextCardVal = deckMapping[nextCard.value];
-          curPlayer.push(nextCardVal);
+          player1.push(nextCardVal);
           //console.log("btnValue is: " + btnValue + "\ncurVal is: " + curVal + "\nnext card val is: " + nextCardVal);
-          $(rowSlot + (cardSlot + 1)).css('background-image', 'url(' + nextCard.image + ')');
-          //console.log(btnValue);
-          if (compareCards(curPlayer, frozenIndex + tempIndex, btnValue)) {
+          $(rowSlot + (cardSlot + 1)).css('background-image', 'url(' + nextCard.image + ')')
+          if (compareCards(player1, frozenIndex + tempIndex, btnValue)) {
             tempIndex++;
+            null;
           } else {
             tempIndex = 0;
-            redrawCardBacks(curPlayer, frozenIndex);
-            curPlayer = curPlayer.slice(0, frozenIndex + 2);
+            redrawCardBacks(frozenIndex + 2);
+            player1 = player1.slice(0, frozenIndex + 1);
           }
           console.log(tempIndex);
+
         });
         break;
       case "freeze":
         console.log("Frozen");
-        frozenIndex = tempIndex;
+        frozenIndex += tempIndex;
         tempIndex = 0;
         break;
       case "replace":
@@ -106,13 +101,3 @@ $.get("https://deckofcardsapi.com/api/deck/" + deckID + "/shuffle/").done(functi
 
   });
 });
-
-// $.get("https://deckofcardsapi.com/api/deck/" + deckID + "/shuffle/");
-// $.get("https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=1").then(function(data) {
-//   console.log(data);
-//   var cards = data.cards;
-//   console.log(cards[0].value, deckMapping[cards[0]["value"]]);
-//
-//
-// });
-// });
